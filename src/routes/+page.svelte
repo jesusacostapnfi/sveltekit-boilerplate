5e6bde5c912c966435504ebc7ffa9f84aed79fe0
jesusacostapnfi <jesus.14acosta.r@gmail.com>
2025-01-27 02:49:16 -0400

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,10 +10,7 @@
 <section>
 	<h1>
 		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
+			
 		</span>
 
 		to your new<br />SvelteKit app
